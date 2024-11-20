2c28254fc5b4353d75a3937fe310a8f4743b3ad8
Harssh <>
2024-11-20 19:46:39 +0530

    Add : Added ros2-humble and gazebo-fortress installation files

---
+++ gazebo-fortress/script.sh
@@ -0,0 +1,2 @@
+# wget "https://gist.githubusercontent.com/Dark-Knight499/45d1cc7012dfff914ecf5726cd014486/raw/ee62a2c4a56e62c12dcf505b0947c6bca1f43d4c/gaazebo-fortess-installation.sh"
+yes | bash gaazebo-fortess-installation.sh
