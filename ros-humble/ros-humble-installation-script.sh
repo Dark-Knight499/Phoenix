e7151cdf119f737efc6009a7dadd89d0b714158f
Harsh Jain <149948139+Dark-Knight499@users.noreply.github.com>
2024-11-21 07:56:44 +0530

    Update and rename script.sh to ros-humble-installation-script.sh

--- ros-humble/ros-humble-installation-script.sh
+++ ros-humble/ros-humble-installation-script.sh
@@ -1,2 +1,3 @@
+#IF RUNNING THIS FILE STAND ALONE UNCOMMENT THE BELOW LINE
 # wget "https://gist.githubusercontent.com/Dark-Knight499/771ae01b20cf4d9c130f1b65504d5e69/raw/8a8b8c5e547a5374fa1089ca6fbe260061be286c/ros2-hubmble-installation.sh"
 yes | ros2-hubmble-installation.sh
